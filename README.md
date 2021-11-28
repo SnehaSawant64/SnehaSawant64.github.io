8cba3bc5bb15b43936105c0462e29ce36218ea4e
Sneha <scsawant1997@gmail.com>
2021-11-28 13:31:22 +0530

    changes

--- README.md
+++ README.md
@@ -3,8 +3,6 @@
   <img alt="Version" src="https://img.shields.io/badge/version-1.0.0-blue.svg?cacheSeconds=2592000" />
 </p>
 
-> This is a personal web page, Made for **fun** just to **learn**
-
 ### 🏠 [Homepage](https://xsneha.github.io )
 
 ## Author
--- src/js/main.js
+++ src/js/main.js
@@ -9,19 +9,11 @@ document.querySelector('body').innerHTML = html`
 </div>
 <main class="main">
 </main>
-
-<!-- <div id="clock"></div> -->
 <footer>
 </footer>
 `;
 
 document.querySelector('main').innerHTML = html`
-<div class="main__text">Simple</div>
-<div class="main__text">but</div>
-<div class="main__text">Not</div>
-<div class="main__text">Ordinary</div>
-<div class="main__break-line"></div>
-<div class="main__text_small">Get ready to <b><i>turn</i></b> your <b><i>ideas</i></b> into <b><i>reality</i></b></div>
 `;
 
 //document.querySelector('nav').innerHTML = html`
