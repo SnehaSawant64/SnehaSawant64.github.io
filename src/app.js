54d65b047631a4101a68653f14d7b9b84c0b06b3
Parikshit <parikshit.s.joshi@gmail.com>
2025-02-11 19:05:33 +0530

    cleaning the workspace

--- src/app.js
+++
@@ -1,8 +0,0 @@
-// SCSS imports
-import './styles/main.scss';
-// Webcomponent imports
-//import 'copyright-x/dist/copyright-x';
-//analog-Clock
-import './js/clock-analog.js';
-// Components
-import './js/main.js';
